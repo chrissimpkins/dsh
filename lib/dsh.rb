54ba10776f018e951d6f3cb5283256c0cc97f3d0
Chris <git.simpkins@gmail.com>
2013-11-04 11:25:20 -0500

    added exception handling for incorrect CL syntax

--- lib/dsh.rb
+++ lib/dsh.rb
@@ -9,4 +9,20 @@ module Dsh
       command = "open dash://" + language + ":" + query
     end
   end #end makeCommand()
+
+  def Dsh.runit(command)
+  	begin
+	    stdout_str, stderr_str, status = Open3.capture3(command)
+	    unless status.exitstatus == 0
+	      STDERR.puts "Unable to launch Dash."
+	      STDERR.puts stderr_str
+	      exit 1
+	    end
+  	rescue => e
+	    STDERR.puts "Kapow! You just blew a gasket. Probably a syntax issue. Please check your options + arguments and try again."
+	    STDERR.puts e.message
+	    exit 1
+	end
+  end #end Dsh.runit()
+
 end #end Dsh module
