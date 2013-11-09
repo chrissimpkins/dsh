73f0a03dc63d844461514a4b379611dae64fa974
Chris <git.simpkins@gmail.com>
2013-11-09 16:41:29 -0500

    added multiple word search query support

--- lib/dsh.rb
+++ lib/dsh.rb
@@ -4,9 +4,9 @@ module Dsh
     # supplemental methods here
   def Dsh.makeCommand(query, language)
     if language.to_s == ''
-      command = "open dash://" + query
+      command = "open 'dash://" + query + "'"
     else
-      command = "open dash://" + language + ":" + query
+      command = "open 'dash://" + language + ":" + query + "'"
     end
   end #end makeCommand()
 
